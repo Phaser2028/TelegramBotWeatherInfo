3c27bd604ea1923c78edb21e141ea11c5eebf266
Phaser2028 <dima.2000600@gmail.com>
2023-08-22 20:15:59 +0300

    Refactor conditional logic using Stream API and make minor updates

--- src/main/java/org/example/service/TelegramBot.java
+++ src/main/java/org/example/service/TelegramBot.java
@@ -1,68 +1,75 @@
 package org.example.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.example.config.TelegramBotProperties;
+import org.example.service.message.MessageHandler;
 import org.example.util.WeatherData;
-import org.example.config.BotConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-
-
-    private final BotConfig config;
-
+    private final TelegramBotProperties.Bot telegramBotProperties;
 
     private final WeatherData weatherData;
 
+    private final List<MessageHandler> messageHandlers;
 
     @Autowired
-    public TelegramBot(BotConfig config, WeatherData weatherData) {
-        this.config = config;
+    public TelegramBot(TelegramBotProperties telegramBotProperties, WeatherData weatherData, List<MessageHandler> messageHandlers){
+        this.telegramBotProperties = telegramBotProperties.getBot();
         this.weatherData = weatherData;
+        this.messageHandlers = messageHandlers;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        long chatId = update.getMessage().getChatId();
 
+        messageHandlers.stream()
+                .filter(handler -> update.hasMessage())
+                .filter(handler -> update.getMessage().hasText())
+                .filter(handler -> handler.canHandle(update.getMessage()))
+                .findFirst()
+                .ifPresentOrElse(
+                        command -> sendMessage(command.handle(update.getMessage())),
+                        () -> {
+                            try {
+                                sendLocation(weatherData.getCoordinates(update.getMessage()));
+                                sendMessage(weatherData.getWeatherInfo(update.getMessage()));
+                            } catch (JsonProcessingException | NullPointerException e) {
+                                sendMessageError(update.getMessage());
+                            }
+                        }
+                );
+    }
 
-        if (update.hasMessage() && update.getMessage().hasText()) {
-
-            if (update.getMessage().getText().equals("/start"))
-                sendMessage(chatId, """
-                        Я бот, который скидывает вам прогноз погоды.
-                        Вы можете отправлять мне как название вашего города, так и название города + название улицы.
-                        Пример: Москва улица Пушкина 41.
-                        Информация о погоде может быть не точной.
-
-                        Разработчик: @Teigt""");
-            else if (update.getMessage().getText().equals("/help"))
-                sendMessage(chatId, "Просто отправьте мне сообщение с названием города или с названием города + названием улицы, и я отправлю вам информацию о погоде");
-
-            else {
-                try {
-                    sendMessage(chatId, weatherData.getWeatherInfo(update.getMessage().getText()));
-                } catch (JsonProcessingException e) {
-                    sendMessage(chatId, "произошла ошибка");
-                    throw new RuntimeException(e);
-                }
-            }
-
+    private void sendMessage(SendMessage incomingMessage) {
+        try {
+            execute(incomingMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
-    private void sendMessage(long chatId, String text) {
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText(text);
+    private void sendLocation(SendLocation incomingMessage) {
+        try {
+            execute(incomingMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessageError(Message incomingMessage) {
+        SendMessage message = new SendMessage(incomingMessage.getChatId().toString(), "Произошла ошибка. Попробуйте изменить название города или подождать.");
         try {
             execute(message);
         } catch (TelegramApiException e) {
@@ -72,11 +79,11 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return config.getBotName();
+        return telegramBotProperties.getName();
     }
 
     @Override
     public String getBotToken() {
-        return config.getToken();
+        return telegramBotProperties.getToken();
     }
 }
